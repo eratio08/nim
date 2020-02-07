554db867471d244dfdb606c87864fdfab71337e7
Eike Lurz <eike.lurz@tuta.io>
2020-02-07 17:09:25 +0100

    fix: resolves issue with random pick

--- src/main/kotlin/de/elurz/nim/strategy/RandomPick.kt
+++ src/main/kotlin/de/elurz/nim/strategy/RandomPick.kt
@@ -9,7 +9,7 @@ object RandomPick : GameStrategy {
     override fun computePins(remainingPins: Int): Int {
         val randomPins: Int = (Math.random() * 10).toInt() % 3 + 1
         val diff = remainingPins - randomPins
-        return if (diff < 0) randomPins - remainingPins else randomPins
+        return if (diff < 0) remainingPins else randomPins
     }
 
     override fun toString(): String {
